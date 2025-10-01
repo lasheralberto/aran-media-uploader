d4517b2517683bd6f134da6a45e8cced75bda3aa
Alberto Martinez Lasheras <72318269+lasheralberto@users.noreply.github.com>
2025-10-01 20:11:08 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Gallery from './components/Gallery';
 
+
 const App: React.FC = () => {
     // The user ID is hardcoded as per the requirements for this specific app instance.
     return <Gallery userId="lasher" />;
