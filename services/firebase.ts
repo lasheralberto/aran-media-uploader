0ce487db3cd3aa9e9183eb9108046ac632df18e3
Alberto Martinez Lasheras <72318269+lasheralberto@users.noreply.github.com>
2026-03-30 11:38:24 +0200

    Update Firebase config to use environment variables
    Replaced hardcoded Firebase configuration with environment variables for improved security.

--- services/firebase.ts
+++ services/firebase.ts
@@ -42,12 +42,12 @@ import {
 // for this sensitive information, especially in a production environment.
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAdLbY3STRARMEbN-zHuQvMD8Fi9D54Lec",
-  authDomain: "aran-boda-c39d3.firebaseapp.com",
-  projectId: "aran-boda-c39d3",
-  storageBucket: "aran-boda-c39d3.firebasestorage.app",
-  messagingSenderId: "339208458993",
-  appId: "1:339208458993:web:3a4a06e08143e1e3254f50"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
